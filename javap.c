5f36e8108e7c3fea652969ffd6050725f9929759
phillbush <phillbush@cock.li>
2021-02-25 18:17:51 -0300

    prepare javap.c

--- javap.c
+++ javap.c
@@ -16,11 +16,55 @@ usage(void)
 	exit(1);
 }
 
+/* get attribute with given tag in list of attributes */
+static Attribute *
+getattr(Attribute *attrs, U2 count, AttributeTag tag)
+{
+	U2 i;
+
+	for (i = 0; i < count; i++)
+		if (attrs[i].tag == tag)
+			return &attrs[i];
+	return NULL;
+}
+
+/* check whether constant pool index is valid */
+static int
+validcpindex(ClassFile *class, U2 index)
+{
+	if (index < 1 || index >= class->constant_pool_count)
+		return 0;
+	return 1;
+}
+
+/* print source file name */
+static int
+printsource(ClassFile *class)
+{
+	Attribute *attr;
+
+	if ((attr = getattr(class->attributes, class->attributes_count, SourceFile)) == NULL)
+		return 0;
+	if (!validcpindex(class, attr->info.sourcefile.sourcefile_index))
+		return -1;
+	printf("Compiled from \"%s\"\n", "asda");
+	return 0;
+}
+
+/* print class header */
+static void
+printclass(ClassFile *class)
+{
+	(void)class;
+}
+
 /* print class contents */
 static void
 javap(ClassFile *class)
 {
-	(void)class;
+	printsource(class);
+	printclass(class);
+	printf("}\n");
 }
 
 /* javap: disassemble jclass files */
@@ -31,7 +75,7 @@ main(int argc, char *argv[])
 	int exitval = 0;
 
 	setprogname(argv[0]);
-	while (--argc && (*++argv)[0] == '-') {
+	while (--argc > 0 && (*++argv)[0] == '-') {
 		if (strcmp(*argv, "-c") == 0) {
 			cflag = 1;
 		} else if (strcmp(*argv, "-p") == 0) {
